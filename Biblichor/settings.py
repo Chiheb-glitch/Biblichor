e4872a746faf542ad0193f77402e014d124b93b7
Chiheb-glitch <Chiheb-glitch>
2022-08-18 00:17:33 +0100

    awsdjango2

--- Biblichor/settings.py
+++ Biblichor/settings.py
@@ -86,8 +86,12 @@ AUTH_USER_MODEL= 'accounts.Account'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': 'django_project',
+        'USER': 'username',
+        'PASSWORD': 'pass1234',
+        'HOST': 'localhost',
+        'PORT': '',
     }
 }
 
